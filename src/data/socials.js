626778a777798730eb7129d8ebbb3906cd63745b
Wilbur <wilburstanley.workspace@gmail.com>
2026-03-04 16:28:14 +0800

    updated links

--- src/data/socials.js
+++ src/data/socials.js
@@ -8,27 +8,19 @@ export const socials = [
     description: 'Software Tools and Books'
   },
   {
-    id: 'threads',
-    name: 'Threads',
-    handle: '@brightlabs.university',
-    url: 'https://threads.net/@brightlabs.university',
-    icon: 'Threads', 
-    description: 'Quick thoughts & updates'
-  },
-  {
-    id: 'instagram',
-    name: 'Instagram',
-    handle: '@brightlabs.university',
-    url: 'https://www.instagram.com/brightlabs.university/',
-    icon: 'Instagram',
-    description: 'Daily insights & behind the scenes'
-  },
-  {
     id: 'youtube',
     name: 'YouTube',
     handle: '@BrightLabsUniversity',
     url: 'https://www.youtube.com/@BrightLabsUniversity',
     icon: 'Youtube',
     description: 'DEEP DIVES AND DEMO VIDEOS'
+  },
+  {
+    id: 'threads',
+    name: 'Threads',
+    handle: '@wilburstanley.dev',
+    url: 'https://www.threads.com/@wilburstanley.dev',
+    icon: 'Threads', 
+    description: 'Quick thoughts & updates'
   },
 ];
