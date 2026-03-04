09bed1f2e8d7b87a080e8df07d29bf9a56af9289
Wilbur <wilburstanley.workspace@gmail.com>
2026-03-04 16:37:57 +0800

    added category

--- src/data/profile.js
+++ src/data/profile.js
@@ -1,7 +1,14 @@
 export const profile = {
   name: 'BrightLabs University',
   tagline: 'We offer custom solutions to your business and personal project',
-  categories: ['Web Development', 'Automation Tools', 'Custom Solutions', 'Desktop Development', 'Android Development', 'Digital Books'],
+  categories: [
+    'Web Development', 
+    'Custom Solutions', 
+    'Automation Tools', 
+    'Productivity Tools', 
+    'Desktop Development', 
+    'Digital Books',
+    'Android Development'],
   footerText: 'all links lead to respected spaces',
   updateHandle: `© ${new Date().getFullYear()} BrightLabs University. All rights reserved.`,
 };
